6170a22a21002589359289fcd7eeaf9657850766
SilverMoon17 <50795723+SilverMoon17@users.noreply.github.com>
2023-10-23 08:38:31 +0300

    Added new Entities

---
+++ Endava.TechCourse.BankApp.Domain/Models/CashByCode.cs
@@ -0,0 +1,11 @@
+﻿using Endava.TechCourse.BankApp.Domain.Common;
+
+namespace Endava.TechCourse.BankApp.Domain.Models
+{
+	public class CashByCode : BaseEntity
+	{
+		public string Code { get; set; }
+		public decimal Amount { get; set; }
+		public Currency Currency { get; set; }
+	}
+}
---
+++ Endava.TechCourse.BankApp.Domain/Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using Endava.TechCourse.BankApp.Domain.Common;
+
+namespace Endava.TechCourse.BankApp.Domain.Models
+{
+	public class Transaction : BaseEntity
+	{
+		public Guid SenderId { get; set; }
+		public Guid ReceiverId { get; set; }
+		public Currency Currency { get; set; }
+		public decimal Amount { get; set; }
+	}
+}
