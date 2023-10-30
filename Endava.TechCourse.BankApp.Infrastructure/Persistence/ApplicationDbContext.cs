88686d82696680e01f0e356c6b9c71105c368a4d
SilverMoon17 <50795723+SilverMoon17@users.noreply.github.com>
2023-10-30 17:09:18 +0200

    Added walletController

--- Endava.TechCourse.BankApp.Infrastructure/Persistence/ApplicationDbContext.cs
+++ Endava.TechCourse.BankApp.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -11,5 +11,13 @@ namespace Endava.TechCourse.BankApp.Infrastructure.Persistence
 
 		public DbSet<Wallet> Wallets { get; set; }
 		public DbSet<Currency> Currencies { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Wallet>().HasKey(w => w.Id);
+			modelBuilder.Entity<Currency>().HasKey(c => c.Id);
+
+			base.OnModelCreating(modelBuilder);
+		}
 	}
 }
---
+++ Shared/AddCurrencyDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Endava.TechCourse.BankApp.Shared
+{
+	public class AddCurrencyDTO
+	{
+		public string Name { get; set; }
+		public string CurrencyCode { get; set; }
+		public decimal ChangeRate { get; set; }
+	}
+}
