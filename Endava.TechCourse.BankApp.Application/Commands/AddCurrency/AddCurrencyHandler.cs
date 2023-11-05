e4aa1d1689d9bd52121ab7e658b00e9d9bea966e
SilverMoon17 <50795723+SilverMoon17@users.noreply.github.com>
2023-11-05 09:22:00 +0200

    Added new pages and CQRS pattern impmlement

---
+++ Endava.TechCourse.BankApp.Application/Commands/AddCurrency/AddCurrencyHandler.cs
@@ -0,0 +1,42 @@
+﻿using Endava.TechCourse.BankApp.Domain.Models;
+using Endava.TechCourse.BankApp.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Endava.TechCourse.BankApp.Application.Commands.AddCurrency
+{
+	public class AddCurrencyHandler : IRequestHandler<AddCurrencyCommand, CommandStatus>
+	{
+		private readonly ApplicationDbContext _context;
+
+		public AddCurrencyHandler(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<CommandStatus> Handle(AddCurrencyCommand request, CancellationToken cancellationToken)
+		{
+			if (await _context.Currencies.AnyAsync(x => x.Name == request.Name, default))
+			{
+				return CommandStatus.Failed("Currency with this name already exists");
+			}
+
+			if (await _context.Currencies.AnyAsync(x => x.CurrencyCode == request.CurrencyCode, default))
+			{
+				return CommandStatus.Failed("Currency with this currency code already exists");
+			}
+
+			var currency = new Currency()
+			{
+				Name = request.Name,
+				CurrencyCode = request.CurrencyCode,
+				ChangeRate = request.ChangeRate
+			};
+
+			await _context.Currencies.AddAsync(currency, cancellationToken);
+			await _context.SaveChangesAsync(cancellationToken);
+
+			return new CommandStatus();
+		}
+	}
+}
